ed216bc1dc9981bd48633435ea9fdd2269c882a6
Eo (PC EO) <weez12@ajou.ac.kr>
2026-07-20 14:08:00 +0900

    첫 프로젝트 생성

---
+++ suwon-now/src/components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <h1>SUWON NOW</h1>
+      </div>
+
+      <nav>
+        <a href="#">홈</a>
+        <a href="#">경기</a>
+        <a href="#">공연</a>
+        <a href="#">축제</a>
+        <a href="#">팝업</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ suwon-now/src/components/Hero.tsx
@@ -0,0 +1,18 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h2>수원의 스포츠와 문화를 한눈에</h2>
+
+        <p>
+          경기 일정, 공연, 축제, 팝업스토어 정보를
+          쉽고 빠르게 확인해보세요.
+        </p>
+
+        <button>이번 달 일정 보기</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
